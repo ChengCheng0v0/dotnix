d6622fed3b7753fbc15747e0127fc525094d0f6b
成成0v0 <chengcheng@miao.ms>
2025-01-31 07:05:38 +0800

    feat(libs): 实现 getSecret 扩展函数
    #[host(libs)]
    #[module("extra-builtins")]

--- libs/extra-builtins.nix
+++ libs/extra-builtins.nix
@@ -1,8 +1,12 @@
 { exec, ... }: {
+  # 解密 Sops 机密文件并将其作为 Nix 表达式完整返回
   readSops = path: builtins.fromJSON (exec [
     "sh" "-c" ''
       echo -n \'\'$(env SOPS_AGE_KEY_FILE=/var/lib/sops/age-key.txt sops -d ${path})\'\'
     ''
   ]);
-  # getSecrets = name: name;
+
+  # 通过 sops-nix 创建的解密后文件获取指定机密的明文
+  # WARNING: 使用此函数需要确保已正确配置 sops-nix；无法获取未声明的机密项。
+  getSecret = name: builtins.readFile "/run/secrets/${name}";
 }
