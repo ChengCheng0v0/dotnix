cf9e27a46edc7a58efffe7f2655dbe08fb056809
成成0v0 <chengcheng@miao.ms>
2025-01-31 09:24:56 +0800

    feat(wallpaper): 新增1张视频壁纸
    #[host("Cheng-NixOS-PC")]
    #[home("chengcheng_0v0")]
    #[module("desktop/wallpaper")]

--- home/chengcheng_0v0@Cheng-NixOS-PC/desktop/wallpaper/default.nix
+++ home/chengcheng_0v0@Cheng-NixOS-PC/desktop/wallpaper/default.nix
@@ -1,8 +1,11 @@
 { config, ... }:
 
 let
-  wpPicturePath = "/etc/nixos/home/chengcheng_0v0@Cheng-NixOS-PC/desktop/wallpaper/picture";
-  wpLinkTarget = "Pictures/wallpaper";
+  wpAssetsPath.picture = "/etc/nixos/home/chengcheng_0v0@Cheng-NixOS-PC/desktop/wallpaper/picture";
+  wpLinkTarget.picture = "Pictures/wallpaper";
+  wpAssetsPath.video = "/etc/nixos/home/chengcheng_0v0@Cheng-NixOS-PC/desktop/wallpaper/video";
+  wpLinkTarget.video = "Videos/wallpaper";
 in {
-  home.file.${wpLinkTarget}.source = config.lib.file.mkOutOfStoreSymlink wpPicturePath;
+  home.file.${wpLinkTarget.picture}.source = config.lib.file.mkOutOfStoreSymlink wpAssetsPath.picture;
+  home.file.${wpLinkTarget.video}.source = config.lib.file.mkOutOfStoreSymlink wpAssetsPath.video;
 }
