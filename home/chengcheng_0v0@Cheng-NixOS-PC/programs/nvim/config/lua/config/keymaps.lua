f146346ef9be34de9c6e9d1baa7b2db1df887cae
成成0v0 <chengcheng@miao.ms>
2025-02-08 10:35:12 +0800

    Revert "feat(kitty): 实现与 Neovim 的窗口聚焦操作互通"
    This reverts commit 5bc0eacdba75b9abc5ef12e0c128cca39dd0a84c.

--- home/chengcheng_0v0@Cheng-NixOS-PC/programs/nvim/config/lua/config/keymaps.lua
+++ home/chengcheng_0v0@Cheng-NixOS-PC/programs/nvim/config/lua/config/keymaps.lua
@@ -12,16 +12,3 @@ local map = vim.api.nvim_set_keymap
 map("n", "(", "^", { noremap = true, silent = true })
 -- 普通模式下按 ) 移动光标至行尾
 map("n", ")", "$", { noremap = true, silent = true })
-
--- Kitty 交互相关的键绑定
-if os.getenv("TERM") == "xterm-kitty" then
-  vim.g.kitty_navigator_no_mappings = 1
-  vim.g.tmux_navigator_no_mappings = 1
-
-  vim.cmd([[
-    noremap <silent> <c-h> :<C-U>KittyNavigateLeft<cr>
-    noremap <silent> <c-l> :<C-U>KittyNavigateRight<cr>
-    noremap <silent> <c-j> :<C-U>KittyNavigateDown<cr>
-    noremap <silent> <c-k> :<C-U>KittyNavigateUp<cr>
-  ]])
-end
