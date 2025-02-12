6c3da68756305b0df0e733581235620baa67a309
成成0v0 <chengcheng@miao.ms>
2025-02-13 00:16:47 +0800

    feat(hardware): 更改 Files 磁盘的挂载点
    #[host("Cheng-NixOS-PC")]
    #[module("hardware-configuration")]

--- hosts/Cheng-NixOS-PC/hardware-configuration.nix
+++ hosts/Cheng-NixOS-PC/hardware-configuration.nix
@@ -26,7 +26,7 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-    "/mnt" = {
+    "/mnt/files" = {
       device = "/dev/disk/by-uuid/4a0f26fa-3cf5-4ee2-a555-3f45c59446b1";
       fsType = "btrfs";
     };
