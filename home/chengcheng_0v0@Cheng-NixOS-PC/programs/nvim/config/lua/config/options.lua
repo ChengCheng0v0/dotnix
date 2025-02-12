46297dbcc1e9bd9f392fb7a41e4aa30976f3290a
成成0v0 <chengcheng@miao.ms>
2025-02-12 22:47:49 +0800

    feat(nvim): 关闭系统剪切板同步
    #[host("Cheng-NixOS-PC")]
    #[home("chengcheng_0v0")]
    #[module("programs/nvim")]

--- home/chengcheng_0v0@Cheng-NixOS-PC/programs/nvim/config/lua/config/options.lua
+++ home/chengcheng_0v0@Cheng-NixOS-PC/programs/nvim/config/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- 关闭系统剪切板同步
+vim.opt.clipboard = ""
+
 -- 启用持久化撤销
 vim.opt.undofile = true
 
