4efd8874a9af758e138d1a0064a4bdbd3bf7fd1b
成成0v0 <chengcheng@miao.ms>
2025-02-13 15:17:23 +0800

    feat(mpv): 新增 uosc 配置
    #[host("Cheng-NixOS-PC")]
    #[home("chengcheng_0v0")]
    #[module("programs/mpv")]

--- home/chengcheng_0v0@Cheng-NixOS-PC/programs/mpv/default.nix
+++ home/chengcheng_0v0@Cheng-NixOS-PC/programs/mpv/default.nix
@@ -1,6 +1,19 @@
+{ pkgs, ... }:
+
 {
   programs.mpv = {
     enable = true;
+    config = {
+      osd-bar = false;
+      border = false;
+    };
+
+    scripts = with pkgs.mpvScripts; [
+      (uosc.overrideAttrs { # 交互式 UI
+        version = "5.8.0";
+      })
+      thumbfast # 缩略图生成器
+    ];
   };
 
   catppuccin.mpv.enable = true;
