c0349f86cde83499715c19fa75e34a3039d77a88
成成0v0 <chengcheng@miao.ms>
2025-02-17 17:56:01 +0800

    feat(spotifyd): 弃用 SpotifyD
    #[host("Cheng-NixOS-PC")]
    #[home("chengcheng_0v0")]
    #[module("services/spotifyd")]

--- home/chengcheng_0v0@Cheng-NixOS-PC/programs/spotify-player/default.nix
+++ home/chengcheng_0v0@Cheng-NixOS-PC/programs/spotify-player/default.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       client_id = "e63e1cea598c4be1b158e5aca12ec0ed";
-      default_device = "Cheng-NixOS-PC";
+      # default_device = "Cheng-NixOS-PC";
     };
   };
 
