d7e6189923c6a6216211ce64811d754276c934fe
成成0v0 <chengcheng@miao.ms>
2025-02-22 23:17:02 +0800

    feat(server-m710q): 新增一些基本软件包

--- hosts/server-m710q/packages.nix
+++ hosts/server-m710q/packages.nix
@@ -2,7 +2,29 @@
 
 {
   environment.systemPackages = with pkgs; [
-    curl
-    git
+    # 系统工具
+    wget        # 从互联网获取文件
+    unzip       # Zip 解压工具
+    age         # 加密工具
+    sops        # 机密管理器
+    jq          # JSON 处理器
+
+    # 语言支持
+    gcc         # GNU 编译器集合
+    clang       # C 语言编译器
+    rustc       # Rust 语言编译器
+    cargo       # Rust 的包管理器
+    python314   # Python 3.14
+    nodejs_23   # Node.js 23
+
+    # 开发工具
+    lunarvim    # 代码编辑器
+    git         # 版本控制系统
+
+    # 网络服务
+    caddy       # HTTP 服务器
+
+    # 其它
+    kitty       # 终端模拟器
   ];
 }
