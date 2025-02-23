47cb8f5c31d909bcb016c67b917f8e224c820b32
成成0v0 <chengcheng@miao.ms>
2025-02-24 00:52:03 +0800

    fix(wallpapers): 修复挂载点配置错误的问题

--- home/chengcheng_0v0@Cheng-NixOS-PC/desktop/wallpapers/default.nix
+++ home/chengcheng_0v0@Cheng-NixOS-PC/desktop/wallpapers/default.nix
@@ -4,7 +4,7 @@ let
   wp = vars.home.chengcheng_0v0.wallpapers;
 in {
   # home.file.${wp.linkTarget.picture}.source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/chengcheng_0v0@Cheng-NixOS-PC/desktop/wallpapers/picture";
-  xdg.configFile.${wp.linkTarget.picture}.source = ./picture;
+  home.file.${wp.linkTarget.picture}.source = ./picture;
   # home.file.${wp.linkTarget.video}.source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/chengcheng_0v0@Cheng-NixOS-PC/desktop/wallpapers/video";
-  xdg.configFile.${wp.linkTarget.video}.source = ./video;
+  home.file.${wp.linkTarget.video}.source = ./video;
 }
