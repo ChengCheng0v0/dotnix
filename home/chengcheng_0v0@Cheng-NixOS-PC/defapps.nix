abcd2fa7b95b363ab0ad0e0a47bc1bb2cfcddeb7
成成0v0 <chengcheng@miao.ms>
2025-03-02 20:21:44 +0800

    feat: 将 qimgv 设为默认的图像查看器

--- home/chengcheng_0v0@Cheng-NixOS-PC/defapps.nix
+++ home/chengcheng_0v0@Cheng-NixOS-PC/defapps.nix
@@ -10,6 +10,7 @@
   };
 
   # MIME 类型
+  xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
     # 终端
     "x-scheme-handler/terminal" = [ "kitty.desktop" ];
@@ -21,5 +22,12 @@
 
     # 文件管理器
     "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+
+    # 图像查看器
+    "image/png" = [ "qimgv.desktop" ];
+    "image/jpeg" = [ "qimgv.desktop" ];
+    "image/webp" = [ "qimgv.desktop" ];
+    "image/gif" = [ "qimgv.desktop" ];
+    "video/webm" = [ "qimgv.desktop" ];
   };
 }
