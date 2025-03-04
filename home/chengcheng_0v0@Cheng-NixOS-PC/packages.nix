c125461fa588f103a77f54673dced6302762e170
成成0v0 <chengcheng@miao.ms>
2025-03-04 16:59:25 +0800

    style(pkgs): 整理分类

--- home/chengcheng_0v0@Cheng-NixOS-PC/packages.nix
+++ home/chengcheng_0v0@Cheng-NixOS-PC/packages.nix
@@ -5,20 +5,6 @@
     # dotnix
     dotnix.wallpaper_picker # 壁纸选择器
 
-    # 终端工具
-    neofetch    # 经典身份证
-    fastfetch   # 时尚身份证
-    eza         # ls 替代品
-    nmap        # 网络安全工具
-    zoxide      # 更智能的 cd
-    usql        # 通用的 SQL 命令行接口
-    thefuck     # 爆炒命令行
-
-    # 桌面环境
-    swww        # 壁纸守护进程
-    grim        # Wayland 截图工具
-    slurp       # Wayland 屏幕选择器
-
     # 语言支持
     rust-analyzer # Rust 分析器
     rustfmt     # Rust 代码格式化工具
@@ -27,15 +13,29 @@
     commitizen  # Git 提交格式化工具
     just        # 命令执行器
     beekeeper-studio # SQL 管理器
+    usql        # 通用的 SQL 命令行接口
 
-    # 实用工具
-    pot         # 派了个萌的翻译器
-    qimgv       # 图像查看器
+    # 终端工具
+    neofetch    # 经典身份证
+    fastfetch   # 时尚身份证
+    eza         # ls 替代品
+    nmap        # 网络安全工具
+    zoxide      # 更智能的 cd
+    thefuck     # 爆炒命令行
     lnav        # 日志文件导航器
     sysz        # systemctl 的 Fzf TUI
     gpg-tui     # GnuPG 的 TUI 管理器
-    clipse      # TUI 剪切板管理器
     termscp     # TUI 文件传输工具
+
+    # 桌面环境
+    swww        # 壁纸守护进程
+    grim        # Wayland 截图工具
+    slurp       # Wayland 屏幕选择器
+    clipse      # TUI 剪切板管理器
+
+    # 桌面工具
+    pot         # 派了个萌的翻译器
+    qimgv       # 图像查看器
 
     # 媒体播放
     pavucontrol # 音频控制
